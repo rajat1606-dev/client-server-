2093ec42f34ad9fe8e00aba54a3df2364a0c485f
rajat1606-dev <20240802728@dypiu.ac.in>
2026-05-05 08:56:28 +0530

    server.py

--- server.py
+++ server.py
@@ -1,25 +1,17 @@
 import socket
 
 # Step 1: Create socket
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Step 2: Bind IP and port
-server_socket.bind(("localhost", 12345))
+# Step 2: Connect to server
+client_socket.connect(("127.0.0.1", 12345))
 
-# Step 3: Listen for client
-server_socket.listen(1)
-print("Server is waiting for connection...")
+# Step 3: Send message
+client_socket.send("Hello from client".encode())
 
-# Step 4: Accept connection
-conn, addr = server_socket.accept()
-print("Connected to:", addr)
+# Step 4: Receive reply
+data = client_socket.recv(1024).decode()
+print("Server says:", data)
 
-# Step 5: Receive message
-data = conn.recv(1024).decode()
-print("Client says:", data)
-
-# Step 6: Send reply
-conn.send("Hello from server".encode())
-
-# Step 7: Close connection
-conn.close()
+# Step 5: Close connection
+client_socket.close()
